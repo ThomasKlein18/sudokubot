27fad0e4739da2795a4b1bf0789f83cc66c917e5
ThomasKlein18 <thomasklein18@gmail.com>
2019-07-25 22:20:39 +0200

    still no better solution for removing grid lines

--- sudoku_vision.py
+++ sudoku_vision.py
@@ -9,11 +9,16 @@ import matplotlib.pyplot as plt
 from skimage.measure import label as label_func
 from skimage.measure import regionprops
 from skimage.transform import hough_line, hough_line_peaks
-from scipy.ndimage import sobel, rotate
+from scipy.signal import convolve2d as convolve2d
+from scipy.ndimage import sobel, rotate, gaussian_filter
 from scipy.ndimage.morphology import binary_erosion, binary_opening
 from PIL import Image, ExifTags
+from scipy import fftpack
 
 from sudoku_solve import print_sudoku
+
+model = tf.keras.models.load_model("checkpoint.h5")
+
 
 def parse_photo(filename):
     """
@@ -22,7 +27,7 @@ def parse_photo(filename):
     returns: matrix of integers
     """
 
-    # step 1: read image (rotate if rotated)
+    # step 1: read image (rotate if necessary)
     img = Image.open(filename)
     if( not img._getexif() is None):
         exif = { ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS }
@@ -32,22 +37,28 @@ def parse_photo(filename):
     img = np.array(img, dtype=np.int)
 
     # step 2: binarize
-    def rgb2gray(rgb):
-        return np.dot(rgb[...,:3], [0.2989, 0.5870, 0.1140])
-
     bin_img = np.sum(img, axis=2) 
-    bin_img = np.invert(bin_img > 140)
-    # bin_img = rgb2gray(img)
+    bin_img = np.invert(bin_img > np.mean(bin_img))
+
+    sobel_vert = sobel(bin_img, 1) # obtain vertical edges
+    sobel_hori = sobel(bin_img, 0) # obtain horizontal edges
+    sobel_img = sobel_vert + sobel_hori
+
+    plt.imshow(sobel_img)
+    plt.show()
 
     # step 3: find largest connected region = sudoku
-    labelled = label_func(bin_img)
+    labelled = label_func(sobel_img)
     props = regionprops(labelled)
     maxval = 0
     sudoku_patch = None
     for prop in props:
-        if prop.area > maxval:
-            maxval = prop.area 
+        minrow, mincol, maxrow, maxcol = prop.bbox
+        area = (maxrow-minrow)*(maxcol-mincol)
+        if area > maxval:
+            maxval = area 
             sudoku_patch = prop
+
     minrow, mincol, maxrow, maxcol = sudoku_patch.bbox
     margin = 0
     img = img[minrow-margin:maxrow+margin, mincol-margin:maxcol+margin]
@@ -55,10 +66,8 @@ def parse_photo(filename):
     plt.imshow(img)
     plt.show()
 
-    sobel_img = sobel(bin_img, 1) # obtain vertical edges
-
     # step 4: find angle and rotate image by that angle
-    h_space, angles, dist = hough_line(sobel_img)
+    h_space, angles, dist = hough_line(sobel_vert)
     h_space, angles, dist = hough_line_peaks(h_space, angles, dist, num_peaks=1)
     angle = math.degrees(angles[0])
     
@@ -72,23 +81,25 @@ def parse_photo(filename):
 
     # step 6: use ANN to recognize digits
     sudoku = np.zeros((9,9))
-    model = tf.keras.models.load_model("checkpoint.h5")
+    global model
         
     for tile, i, j in tiles:
 
         m,n,_ = tile.shape
 
         tile = np.sum(tile, axis=2) 
-        tile = np.invert(tile > 140).astype(int)
+        tile = np.invert(tile > np.mean(tile)*0.7).astype(int)
 
         for k in range(m):
-            if np.sum(tile[k,:]) > 0.7*m:
+            if np.sum(tile[k,:]) > 0.6*m:
                 tile[k,:] = 0
         for k in range(n):
-            if np.sum(tile[:,k]) > 0.7 * n:
+            if np.sum(tile[:,k]) > 0.6*n:
                 tile[:,k] = 0
-        
+        #opened_tile = tile
         opened_tile = binary_opening(tile, np.ones((3,3))).astype(int)
+        plt.imshow(opened_tile)
+        plt.show()
 
         if np.sum(opened_tile) / (opened_tile.shape[0]*opened_tile.shape[1]) > 0.01:
 
@@ -102,14 +113,19 @@ def parse_photo(filename):
                     number_patch = prop
             minrow, mincol, maxrow, maxcol = number_patch.bbox
             margin = 4
+            
+            # pad image with zeros to make it more MNIST-like
             tile = np.pad(tile[minrow:maxrow, mincol:maxcol], [[margin,margin],[2*margin,2*margin]], mode="constant")
             
             tile = cv2.resize(tile.astype(np.uint8), dsize=(28, 28), interpolation=cv2.INTER_CUBIC)
-            
+            plt.imshow(tile)
+            plt.show()
             predictions = model.predict(np.reshape(tile, [1,28,28,1]))[0]
             label = np.argmax(predictions)
 
             sudoku[i,j] = label
+
     return sudoku.astype(np.uint8)
 
-print_sudoku(parse_photo("sudoku_imgs/f.jpg"))
+if __name__ == "__main__":
+    print_sudoku(parse_photo("sudoku_imgs/b.jpg"))
